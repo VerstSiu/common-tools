6b7b83eb98cd0dd3b477139180092326acd174bb
verstsiu <verstsiu@126.com>
2019-05-05 16:59:44 +0800

    :sparkles: Add JsonNode verifyAsXX extensions

---
+++ jackson/src/main/kotlin/com/ijoic/tools/jackson/_parse.kt
@@ -0,0 +1,82 @@
+package com.ijoic.tools.jackson
+
+import com.fasterxml.jackson.databind.JsonNode
+
+/* -- current node extensions :begin -- */
+
+/**
+ * Verify current node as string or null
+ */
+fun JsonNode.verifyAsString(): String? {
+  if (this.isNull) {
+    return null
+  }
+  return this.asText()
+}
+
+/**
+ * Verify current node as string or null
+ */
+fun JsonNode.verifyAsInt(): Int? {
+  if (this.isNull) {
+    return null
+  }
+  if (!this.canConvertToInt()) {
+    return null
+  }
+  return this.intValue()
+}
+
+/**
+ * Verify current node as object
+ */
+fun JsonNode.verifyAsObject(): JsonNode? {
+  if (!this.isObject) {
+    return null
+  }
+  return this
+}
+
+/**
+ * Verify current node as array
+ */
+fun JsonNode.verifyAsArray(): JsonNode? {
+  if (!this.isArray) {
+    return null
+  }
+  return this
+}
+
+/* -- current node extensions :end -- */
+
+/* -- field node extensions :begin -- */
+
+/**
+ * Verify [field] node as string or null
+ */
+fun JsonNode.verifyAsString(field: String): String? {
+  return get(field)?.verifyAsString()
+}
+
+/**
+ * Verify [field] node as string or null
+ */
+fun JsonNode.verifyAsInt(field: String): Int? {
+  return get(field)?.verifyAsInt()
+}
+
+/**
+ * Verify [field] node as object
+ */
+fun JsonNode.verifyAsObject(field: String): JsonNode? {
+  return get(field)?.verifyAsObject()
+}
+
+/**
+ * Verify [field] node as array
+ */
+fun JsonNode.verifyAsArray(field: String): JsonNode? {
+  return get(field)?.verifyAsArray()
+}
+
+/* -- field node extensions :end -- */
